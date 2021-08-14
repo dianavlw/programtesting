951aa1ec2bbd2ea21678629ed311523fda3b5b2a
dianavlw <dianavlw@gmail.com>
2021-08-13 22:37:04 -0700

    arrays and string cracking the code interview

--- ch1.py
+++ ch1.py
@@ -1,35 +1,35 @@
 
-# is_unique
+# 1.1 is_unique
 
-# def is_unique(str):
-#     arr = [False] * 128
-#     for char in str:
-#         index = ord(char)
-#         if arr[index]:
-#             return False
-#         arr[index] = True
-#     return True
+def is_unique(str):
+    arr = [False] * 128
+    for char in str:
+        index = ord(char)
+        if arr[index]:
+            return False
+        arr[index] = True
+    return True
 
-# def is_unique_sorting(str):
-#     sorted_string = sorted(str)
-#     last_character = None
-#     for char in sorted_string:
-#         if char == last_character:
-#             return False
-#         last_character = char
-#     return True
+def is_unique_sorting(str):
+    sorted_string = sorted(str)
+    last_character = None
+    for char in sorted_string:
+        if char == last_character:
+            return False
+        last_character = char
+    return True
 
-# print(is_unique("yolo") == False)
-# print(is_unique("rad") == True)
-# print(is_unique("yOlo") == True)
+print(is_unique("yolo") == False)
+print(is_unique("rad") == True)
+print(is_unique("yOlo") == True)
 
 
-# print(is_unique_sorting("yolo") == False)
-# print(is_unique_sorting("rad") == True)
-# print(is_unique_sorting("yOlo") == True)
+print(is_unique_sorting("yolo") == False)
+print(is_unique_sorting("rad") == True)
+print(is_unique_sorting("yOlo") == True)
 
 """ 
-Given two string, write a method to decide if one is a permutation of the other. 
+1.2 Given two string, write a method to decide if one is a permutation of the other. 
 Can you assume that it isnt a permutation char if one is capital vs a lowercase 
 Can you also assume that it doesnt acknoledge spaces making it not a permutation
 """
@@ -46,49 +46,49 @@ Can you also assume that it doesnt acknoledge spaces making it not a permutation
 #sort them alphabetically 
 # also join the str if there are spaces 
 
-# def is_perm_1(str_1, str_2):
-#     str_1 = str_1.lower()
-#     str_2 = str_2.lower()
+def is_perm_1(str_1, str_2):
+    str_1 = str_1.lower()
+    str_2 = str_2.lower()
 
-#     if len(str_1) != len(str_2):
-#         return False
-#     str_1 = ''.join(sorted(str_1))
-#     str_2 = ''.join(sorted(str_2))
+    if len(str_1) != len(str_2):
+        return False
+    str_1 = ''.join(sorted(str_1))
+    str_2 = ''.join(sorted(str_2))
 
-#     n = len(str_1)
-#     for i in range(n):
-#         if str_1[i] != str_2[i]:
-#             return False
-#     return True
-# print(is_perm_1(is_permutation_1, is_permutation_2))
-# print(is_perm_1(not_permutation_1, not_permutation_2))
-
-"""
-    URLify: replace all the spaces with "%20" assume that the string has sufficient space
+    n = len(str_1)
+    for i in range(n):
+        if str_1[i] != str_2[i]:
+            return False
+    return True
+print(is_perm_1(is_permutation_1, is_permutation_2))
+print(is_perm_1(not_permutation_1, not_permutation_2))
 
 """
+    1.3 URLify: replace all the spaces with "%20" assume that the string has sufficient space
 
-# def urlify(s, size):
-#     s = s[0:size]
-#     return s.replace(" ", "%20")
+"""
 
-# def urlify2(s, size):
-#     new_string = []
+def urlify(s, size):
+    s = s[0:size]
+    return s.replace(" ", "%20")
 
-#     for char in s[0:size]:
-#         if char == " ":
-#             temp = "%20"
-#         else:
-#             temp = char
-#         new_string.append(temp)
-#     converted_string = "".join(new_string)
-#     return converted_string
+def urlify2(s, size):
+    new_string = []
 
-# # Driver Code
-# s = 'Mr John Smith'
-# size = 13
-# print(urlify(s, size))
-# print(urlify2(s, size))
+    for char in s[0:size]:
+        if char == " ":
+            temp = "%20"
+        else:
+            temp = char
+        new_string.append(temp)
+    converted_string = "".join(new_string)
+    return converted_string
+
+# Driver Code
+s = 'Mr John Smith'
+size = 13
+print(urlify(s, size))
+print(urlify2(s, size))
 
 """
     1.4 Palindrom Permutation: given a string write a function to check if it is a permutation of a palindrome.  A palindrome is a word which is written the same forwards and backwards EX. 'madam' 'racecar' kayak'. a permutation is an arrangement of letters EX "god" "dog"
@@ -101,8 +101,8 @@ Can you also assume that it doesnt acknoledge spaces making it not a permutation
 def is_palindrome_permutation(s):
     s = s.lower().replace(" ", '')
 
-    # we want to now make a dictionary to place 
-    # we need to keep track of the char count
+    we want to now make a dictionary to place 
+    we need to keep track of the char count
 
     char_set = {}
     odd_count= 0
@@ -129,4 +129,77 @@ m = 'aljw'
 print(is_palindrome_permutation(s))
 print(is_palindrome_permutation(m))
 
-         
+"""
+    1.5 One away: There are 3 types of edits that ca be performed on string: insert character, remove a charater or replace a character. Given two strings, write a function to check if they're one edit(or zero edits) away. 
+"""
+
+# replace: pale, bale => true
+# insert/delete: ple, pale => true
+
+def are_one_edit_different(s1, s2):
+    """Check if a string can converted to another string with a single edit"""
+    if len(s1) == len(s2): #pale, bale replace  the b to p 
+        return one_edit_replace(s1, s2)
+    if len(s1) + 1 == len(s2):#ple, pale insert the a
+        return one_edit_insert(s1, s2)
+    if len(s1) - 1 == len(s2):
+        return one_edit_insert(s2, s1)  
+    return False
+
+
+def one_edit_replace(s1, s2): #pale, bale replace  the b to p  
+    edited = False #nothing was replace 
+    for c1, c2 in zip(s1, s2): # zip takes the first itme passes it and is pared and then the second item is paired, if the passed iterators have different lengths the one with least itmes decides the length
+        if c1 != c2:
+            if edited:
+                return False
+            edited = True
+    return True
+
+
+def one_edit_insert(s1, s2): #ple, pale insert the a
+    edited = False
+    i, j = 0, 0
+    while i < len(s1) and j < len(s2):
+        if s1[i] != s2[j]:
+            if edited:
+                return False
+            edited = True
+            j += 1
+        else:
+            i += 1
+            j += 1
+    return True
+
+#Driver Code
+print(are_one_edit_different("pale", "ple")) # True 
+print(are_one_edit_different("palks", "pal")) # False
+print(are_one_edit_different("paleabc", "pleabc")) #True
+print(are_one_edit_different("pale", "pas")) #False 
+
+"""
+    1.6 Compress String: Given a String, return a string with the characters and the count of the characters ex: "aabbcccc" => a2b2c4. If the compressed string is not smaller than the original string, return the original string.
+"""
+
+def stringCompression(string):
+    dic = {}
+    newStr = ''
+
+    for char in string:
+        if not char in dic:
+            dic[char] = 1
+        else:
+            dic[char] += 1
+    
+    for key, value in dic.items():
+        newStr += key
+        newStr += str(value)
+
+    if len(string) == len(newStr):
+        return string
+    else:
+        return newStr
+
+print(stringCompression("aabcccccaaa")) # a5b1c5
+print(stringCompression("aabb")) # aabb will return the original string
+print(stringCompression("aaa")) #a3
