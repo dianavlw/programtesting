ba50dab195081908acb4eb1b0987776c29dd58cf
dianavlw <dianavlw@gmail.com>
2021-08-27 20:08:54 -0700

    leetcode

--- ch3.py
+++ ch3.py
@@ -1,4 +1,68 @@
 """
+class MultiStack:
+    def __init__(self, stack_size, number_of_stacks):
+        self.number_of_stacks = number_of_stacks
+        self.array = [0] * (stack_size * self.number_of_stacks)
+        self.sizes = [0] * self.number_of_stacks
+        self.stack_size = stack_size
+
+    def push(self, value, stack_num):
+        self._assert_valid_stack_num(stack_num)
+        if self.is_full(stack_num):
+            raise StackFullError(f"Push failed: stack #{stack_num} is full")
+        self.sizes[stack_num] += 1
+        self.array[self.index_of_top(stack_num)] = value
+
+    def pop(self, stack_num):
+        self._assert_valid_stack_num(stack_num)
+        if self.is_empty(stack_num):
+            raise StackEmptyError(f"Cannot pop from empty stack #{stack_num}")
+        value = self.array[self.index_of_top(stack_num)]
+        self.array[self.index_of_top(stack_num)] = 0
+        self.sizes[stack_num] -= 1
+        return value
+
+    def peek(self, stack_num):
+        self._assert_valid_stack_num(stack_num)
+        if self.is_empty(stack_num):
+            raise StackEmptyError(f"Cannot peek at empty stack #{stack_num}")
+        return self.array[self.index_of_top(stack_num)]
+
+    def is_empty(self, stack_num):
+        self._assert_valid_stack_num(stack_num)
+        return self.sizes[stack_num] == 0
+
+    def is_full(self, stack_num):
+        self._assert_valid_stack_num(stack_num)
+        return self.sizes[stack_num] == self.stack_size
+
+    def index_of_top(self, stack_num):
+        self._assert_valid_stack_num(stack_num)
+        offset = stack_num * self.stack_size
+        return offset + self.sizes[stack_num] - 1
+
+    def _assert_valid_stack_num(self, stack_num):
+        if stack_num >= self.number_of_stacks:
+            raise StackDoesNotExistError(f"Stack #{stack_num} does not exist")
+
+class MultiStackError(Exception):
+    """multistack operation error"""
+
+class StackFullError(MultiStackError):
+    """the stack is full"""
+
+class StackEmptyError(MultiStackError):
+    """the stack is empty"""
+
+
+class StackDoesNotExistError(ValueError):
+    """stack does not exist"""
+
+
+"""
+
+
+"""
     Chapter 3 of Cracking coding interview by Gaylee L.
 
     3.1 Three to one: Describe how you could use a single array to implement three stacks.
@@ -76,3 +140,20 @@ class MinStack:
      
     def getMin(self):
         return self.minStack[-1]
+
+
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def minDepth(self, root):
+        if not root:
+            return 0
+        else:
+            # if one of the subtree is None, you should return the depth of another subtree.
+            # if all of the subtree is not None, you should return the minimum depth of the two subtrees
+            if root.left is None:
+                return self.minDepth(root.right) + 1
+            elif root.right is None:
+                return self.minDepth(root.left) + 1
+            else:
+                return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
--- permutations.py
+++ permutations.py
@@ -15,7 +15,7 @@ class Solution:
         result = []
         
         if(len(nums) == 1):
-            return [nums[:]]
+            return [nums[:]] # vs copy() this will make your code slow
         
         for i in range(len(nums)):
             n = nums.pop(0)
@@ -23,10 +23,121 @@ class Solution:
             
             for perm in perms:
                 perm.append(n)
-            result.extend(perms)
+            result.extend(perms) #The extend() method adds all the elements of an iterable (list, tuple, string etc.) to the end of the list.
             nums.append(n)
             
         return result
 
 nums = [1,2,3]       
-print(Solution().permute(nums))
+print(Solution().permute(nums)) # expected output = [[3, 2, 1], [2, 3, 1], [1, 3, 2], [3, 1, 2], [2, 1, 3], [1, 2, 3]]
+
+
+ """
+    47. Permutations II
+
+    Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+    
+    Example 1:
+    Input: nums = [1,1,2]
+    Output:
+            [[1,1,2], [1,2,1],[2,1,1]]
+
+    Thought process:
+
+        1,2,3
+        3*2*1 n!
+        
+        3 permutation: list itself is one
+        backtracking
+        hashmap
+        We have two 1's
+        and we have one 2's
+        num |cout
+        ----------
+        1     2
+        _________
+        2     1
+        
+        res = []
+        perm = []
+        count = { n:0 for n in nums}
+        for n in nums:
+            count[n] += 1
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        
+        #:type nums: List[int]
+        #:rtype: List[List[int]]
+                
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return 
+            
+            for n in count:
+                if count[n] > 0:
+                    perm.append(n)
+                    count[n] -= 1
+                    
+                    dfs()
+                    
+                    count[n] += 1
+                    perm.pop()
+                    
+        dfs()
+        return res
+"""
+
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """       
+        res = []
+        nums.sort()
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+        for i in xrange(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+#second solution
+
+class Solution:
+    def __init__(self):
+        self.res = []
+
+    def permute(self, nums):
+        self.backtrack(nums, [])
+        return self.res
+
+    def backtrack(self, nums, path):
+        if not nums:
+            self.res.append(path)
+        for x in range(len(nums)):
+            #[2,3]
+            #path =[1,_,_]
+            self.backtrack(nums[:x] + nums[x+1], path+[nums[x]])
+
+# third solution
+
+def permutations(word):
+    if len(word) == 1:
+        return [word]
+    
+    perms = permutations(word[1:])
+    char = word[0]
+    result = []
+    for perm in perms:
+        for i in range(len(perm) + 1):
+            result.append(perm[:1] + char + perm[i:])
+
+    return result
+print(permutations('abs'))
