36d1153d55b0cd80678fa89007c83adfd516a46c
dianavlw <dianavlw@gmail.com>
2021-09-02 10:45:53 -0700

    090221

--- hackerrank.py
+++ hackerrank.py
@@ -190,3 +190,256 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+    
+ 
+
+
+
+
+
+Print the Elements of a Linked List   
+https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem   
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    temp = head
+    while temp != None:
+        print(temp.data)
+        temp = temp.next
+    
+
+if __name__ == '__main__':    
+    
+    
+Insert a Node at the Tail of a Linked List:
+
+https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+You are given the pointer to the head node of a linked list and an integer to add to the list. Create a new node with the given integer. Insert this node at the tail of the linked list and return the head node of the linked list formed after inserting this new node. The given head pointer may be null, meaning that the initial list is empty.
+
+Function Description
+
+Complete the insertNodeAtTail function in the editor below.
+
+insertNodeAtTail has the following parameters:
+
+SinglyLinkedListNode pointer head: a reference to the head of a list
+int data: the data value for the node to insert
+Returns
+
+SinglyLinkedListNode pointer: reference to the head of the modified linked list
+Input Format
+
+The first line contains an integer , the number of elements in the linked list.
+The next  lines contain an integer each, the value that needs to be inserted at tail.
+
+Constraints
+
+Sample Input
+
+STDIN Function ----- -------- 5 size of linked list n = 5 141 linked list data values 141..474 302 164 530 474
+
+Sample Output
+
+141
+302
+164
+530
+474
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    node = SinglyLinkedListNode(data)
+    #head pointer is null
+    if head == None:
+        head = node
+    else:
+        #insert node at tail
+        temp = head
+        while temp.next != None:
+            temp = temp.next
+        temp.next = node
+        
+    return head
+    
+
+if __name__ == '__main__':
+    
+    
+    
+Insert a node at the head of a linked list
+
+Given a pointer to the head of a linked list, insert a new node before the head. The  value in the new node should point to  and the  value should be replaced with a given value. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
+
+Function Description
+
+Complete the function insertNodeAtHead in the editor below.
+
+insertNodeAtHead has the following parameter(s):
+
+SinglyLinkedListNode llist: a reference to the head of a list
+data: the value to insert in the  field of the new node
+Input Format
+
+The first line contains an integer , the number of elements to be inserted at the head of the list.
+The next  lines contain an integer each, the elements to be inserted, one per function call.
+
+Constraints
+
+Sample Input
+
+5
+383
+484
+392
+975
+321
+Sample Output
+
+321
+975
+392
+484
+383    
+    
+    
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtHead(llist, data):
+    #Create new node
+    node = SinglyLinkedListNode(data)
+    
+    if llist != None:
+        node.next = llist
+    return node
+    
+    
+
+if __name__ == '__main__':    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
